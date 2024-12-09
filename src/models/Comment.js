be6d41172bd727a27bc9322c0643a21a3cfa7a10
席梓乔 <xiziqiao@xiziqiaodeMacBook-Pro.local>
2024-12-09 15:27:34 -0800

    start adding comment feature

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,22 +1,52 @@
-// models/Comment.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Comment = sequelize.define('Comment', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     postId: {
         type: DataTypes.INTEGER,
-        
+        allowNull: false,
+        references: {
+            model: 'posts',
+            key: 'id'
+        },
+        onDelete: 'CASCADE'  // If post is deleted, delete its comments
     },
     userId: {
         type: DataTypes.UUID,
-        
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        },
+        onDelete: 'CASCADE'  // If user is deleted, delete their comments
     },
     content: {
         type: DataTypes.TEXT,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: true  // Prevent empty strings
+        }
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
     }
 }, {
-    tableName: 'comments'
+    tableName: 'comments',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
 });
 
 module.exports = Comment;
